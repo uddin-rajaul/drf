84727639564f95c3ec0d5697e94707762d345c76
Rajaul <uddin.rajaul1@gmail.com>
2023-08-12 09:59:16 +0545

    access items through list

--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import CheckList, CheckListItem
 
-class CheckListSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = CheckList
-        fields = '__all__'
-
 
 class CheckListItemSerializers(serializers.ModelSerializer):
     class Meta:
         model = CheckListItem
+        fields = '__all__'
+
+
+class CheckListSerializers(serializers.ModelSerializer):
+    items = CheckListItemSerializers(source='checklistitem_set', many=True, read_only = True)
+    class Meta:
+        model = CheckList
         fields = '__all__'
